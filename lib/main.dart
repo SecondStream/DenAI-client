c519530953c490f1c306767516e55949b18a1627
SecondStream <streamletsplay@gmail.com>
2026-06-02 11:20:47 +0300

    Initialization of windowManager has been moved to a platform-specific condition.

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,9 @@ import 'package:window_manager/window_manager.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await windowManager.ensureInitialized();
 
   if (Platform.isMacOS || Platform.isLinux || Platform.isWindows) {
+    await windowManager.ensureInitialized();
     WindowOptions windowOptions = WindowOptions(
       size: Size(1280, 900),
       center: true,
